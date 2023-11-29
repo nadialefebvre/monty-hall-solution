2f1b13e20a50975d8e27a799824baa2a3cf8ba4b
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-11-29 17:57:23 +0100

    add switchWins and keepWins logic

--- src/main/scala/MontyHallSimulation.scala
+++ src/main/scala/MontyHallSimulation.scala
@@ -21,36 +21,68 @@
     - Display the number of trials performed
     - Show the counts of switch wins and keep wins
     - Calculate and display win percentages for both strategies
-*/
+ */
 
 import scala.util.Random
 
 object MontyHallSimulation extends App {
   val numTrials = 100
 
-  def simulateGame(remainingTrials: Int, random: Random): Unit = {
+  def simulateGame(
+      remainingTrials: Int,
+      switchWins: Int,
+      keepWins: Int,
+      random: Random
+  ): (Int, Int) = {
     if (remainingTrials <= 0) {
-      println("100 trials done")
+      (switchWins, keepWins)
     } else {
       val doorsRange = 1 to 3
 
       val carDoor = doorsRange(random.nextInt(doorsRange.length))
       val chosenDoor = doorsRange(random.nextInt(doorsRange.length))
+      val remainingDoors =
+        doorsRange.filterNot(_ == carDoor).filterNot(_ == chosenDoor)
+      val openedDoor = remainingDoors(
+        random.nextInt(remainingDoors.length)
+      )
+      val switchedDoor =
+        (doorsRange)
+          .filterNot(_ == chosenDoor)
+          .filterNot(_ == openedDoor)
+          .head
 
-      val remainingDoors = doorsRange.filterNot(_ == carDoor).filterNot(_ == chosenDoor)
-      val openedDoor = remainingDoors(random.nextInt(remainingDoors.length))
+      val updatedSwitchWins =
+        if (switchedDoor == carDoor) switchWins + 1 else switchWins
+      val updatedKeepWins =
+        if (chosenDoor == carDoor) keepWins + 1 else keepWins
 
-      val switchedDoor = (doorsRange).filterNot(_ == chosenDoor).filterNot(_ == openedDoor).head
-
-      println(s"$remainingTrials = car door is $carDoor and chosen door is $chosenDoor, remaining doors are $remainingDoors and opened door is $openedDoor and switched door is $switchedDoor")
-      simulateGame(remainingTrials - 1, random)
+      simulateGame(
+        remainingTrials - 1,
+        updatedSwitchWins,
+        updatedKeepWins,
+        random
+      )
     }
   }
 
-  def runTrials(numTrials: Int): (Unit) = {
+  def runTrials(numTrials: Int): (Int, Int) = {
     val random = new Random()
-    simulateGame(numTrials, random)
+    simulateGame(numTrials, 0, 0, random)
   }
 
-  runTrials(numTrials)
+  val (switchWins, keepWins) = runTrials(numTrials)
+
+  println(s"Number of trials in this simulation: $numTrials")
+  println(s"Switching doors wins: $switchWins times")
+  println(s"Keeping initial choice wins: $keepWins times")
+
+  val switchWinPercentage = switchWins.toDouble / numTrials * 100
+  val keepWinPercentage = keepWins.toDouble / numTrials * 100
+
+  println(s"Switching doors win percentage: $switchWinPercentage%")
+  println(
+    s"Keeping initial choice win percentage: $keepWinPercentage%"
+  )
+
 }
