da0298ee09ad731164cdfd9232cf7d7a6fc2cdf3
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-11-30 11:15:13 +0100

    change value naming

--- src/main/scala/MontyHallSimulation.scala
+++ src/main/scala/MontyHallSimulation.scala
@@ -5,22 +5,6 @@
     - A non-chosen door not hiding the car is revealed by the host
     - The player has then the possibility to keep their initial choice or to switch for the non-opened door
     - Is it to their advantage to switch? (short answer: yes, 2/3 probability)
-
-  Set up game variables:
-    - Define the total number of trials to simulate
-    - Create variables to keep track of both numbers of wins for switching and for keeping door
-
-  Define a recursive function to perform the simulation until the number of trials is reached, which will:
-    - Generate the car door and the initially chosen door randomly
-    - Identify the remaining door(s) by filtering out the chosen door and the car door (can be the same door)
-    - Select one door for the host to open among the remaining doors
-    - Calculate the switched door based on the strategy (switch or keep)
-    - Update win counts for both switching and keeping strategies
-
-  Output the simulation results:
-    - Display the number of trials performed
-    - Show the counts of switch wins and keep wins
-    - Calculate and display win percentages for both strategies
  */
 
 import scala.util.Random
@@ -37,14 +21,14 @@ object MontyHallSimulation {
     } else {
       val doorsRange = 1 to 3
 
-      val carDoor = doorsRange(random.nextInt(doorsRange.length))
+      val prizeDoor = doorsRange(random.nextInt(doorsRange.length))
       val chosenDoor = doorsRange(random.nextInt(doorsRange.length))
-      val remainingDoors = doorsRange.filterNot(_ == carDoor).filterNot(_ == chosenDoor)
+      val remainingDoors = doorsRange.filterNot(_ == prizeDoor).filterNot(_ == chosenDoor)
       val openedDoor = remainingDoors(random.nextInt(remainingDoors.length))
       val switchedDoor = doorsRange.filterNot(_ == chosenDoor).filterNot(_ == openedDoor).head
 
-      val updatedSwitchWins = if (switchedDoor == carDoor) switchWins + 1 else switchWins
-      val updatedKeepWins = if (chosenDoor == carDoor) keepWins + 1 else keepWins
+      val updatedSwitchWins = if (switchedDoor == prizeDoor) switchWins + 1 else switchWins
+      val updatedKeepWins = if (chosenDoor == prizeDoor) keepWins + 1 else keepWins
 
       simulateGame(remainingTrials - 1, updatedSwitchWins, updatedKeepWins, random)
     }
